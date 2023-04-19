71c819e4056c4ba82d754e070c16dd585705bf0b
Sy Truong <sy.truong@trackinno.com>
2023-04-19 15:28:01 +0300

    commented out movieRoutes, install new packages, connect with mongoDB, update models with Mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
+// const movieRoutes = require('./routes/movies');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/youtube-video-sharer', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -19,8 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/youtube-v
   .catch((err) => console.error('Could not connect to MongoDB:', err));
 
 // Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/movies', require('./routes/movies'));
+app.use('/api/users', userRoutes);
+// app.use('/api/movies', movieRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    votedMovies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Movie',
+      },
+    ],
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
