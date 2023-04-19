9fb4af36e7cf35be4a8b48b1af789a007f60a537
Sy Truong <sy.truong@trackinno.com>
2023-04-19 15:30:25 +0300

    add userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,62 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Handle user registration
+const registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const userExists = await User.findOne({ username });
+
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ username, password: hashedPassword });
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      token: jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' }),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Handle user login
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      token: jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' }),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
