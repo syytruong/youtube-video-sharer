fcb284457f65db5e35bae3e4eb0363026b13ceec
Sy Truong <sy.truong@trackinno.com>
2023-04-19 17:23:32 +0300

    remove unnecessary import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
