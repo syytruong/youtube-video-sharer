b1669bba804b94411f2a985a5338952c00ec778a
Sy Truong <sy.truong@trackinno.com>
2023-04-19 14:34:18 +0300

    setup App.js with endpoints needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import Header from './components/Header';
+import ShareMovie from './components/ShareMovie';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={MovieList} />
+          <Route path="/share" component={ShareMovie} />
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 }
 
