a98b840565ac6d3a482f3da3d8e01701dbb8816a
Sy Truong <sy.truong@trackinno.com>
2023-04-20 14:15:48 +0300

    add unit test for movie card

---
+++ client/src/__tests__/MovieCard.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable testing-library/no-unnecessary-act */
+import { render, fireEvent, screen, act } from '@testing-library/react';
+import MovieCard from '../components/MovieCard';
+
+const movie = {
+  _id: '1',
+  title: 'Funny Movie 1',
+  youtubeUrl: 'https://www.youtube.com/watch?v=12345',
+  upVotes: 5,
+  downVotes: 3,
+};
+
+const user = {
+  username: 'testUser',
+  token: 'testToken',
+  votedMovies: {},
+};
+
+const handleVote = jest.fn();
+
+describe('MovieCard', () => {
+  it('renders the movie card correctly', () => {
+    render(<MovieCard movie={movie} user={user} handleVote={handleVote} dataTestId="movie-card" />);
+
+    expect(screen.getByTestId('movie-card')).toBeInTheDocument();
+    expect(screen.getByText('Funny Movie 1')).toBeInTheDocument();
+    expect(screen.getByText('Shared by: Deleted user')).toBeInTheDocument();
+    expect(screen.getByTestId('upvote-button-1')).toBeInTheDocument();
+    expect(screen.getByTestId('down-button-1')).toBeInTheDocument();
+  });
+
+  it('handles button clicks', async () => {
+    render(<MovieCard movie={movie} user={user} handleVote={handleVote} dataTestId="movie-card" />);
+
+    await act(async () => {
+      fireEvent.click(screen.getByTestId('upvote-button-1'));
+    });
+    expect(handleVote).toHaveBeenCalledTimes(1);
+    expect(handleVote).toHaveBeenCalledWith('1', 'upVotes');
+    
+    await act(async () => {
+      fireEvent.click(screen.getByTestId('down-button-1'));
+    });
+    expect(handleVote).toHaveBeenCalledTimes(2);
+    expect(handleVote).toHaveBeenCalledWith('1', 'downVotes');
+  });
+});
